232248c3e9c87ffbdd4d206d6789347259520d0f
Sungyong Cho <to.sungyongcho@gmail.com>
2022-05-31 22:48:49 +0200

    added a description on the shell script

--- peda_setup.sh
+++ peda_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+## peda
+## a plugin works with gdb
+# https://github.com/longld/peda
+
+# either this script, or the commands below needs to be executed on each level
 chmod uw+rwx ~
 echo "source /tmp/peda/peda.py" >> ~/.gdbinit
